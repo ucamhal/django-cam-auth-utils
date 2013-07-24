936e81e3f283290c3fc017f8796135d6a67913c2
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-07-24 12:36:24 +0000

    Add __version__ and __version_info__ attrs and use them in setup.py

---
+++ django_cam_auth_utils/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.0.2"
+__version_info__ = tuple(int(n) for n in __version__.split("."))
--- setup.py
+++ setup.py
@@ -2,15 +2,30 @@ from os import path
 
 from setuptools import setup
 
+
 def get_readme_text():
     readme_path = path.abspath(path.join(__file__, "../README.rst"))
     with open(readme_path) as readme:
         return readme.read()
 
+def get_version(filename):
+    """
+    Parse the value of the __version__ var from a Python source file
+    without running/importing the file.
+    """
+    import re
+    version_pattern = r"^ *__version__ *= *['\"](\d+\.\d+\.\d+)['\"] *$"
+    match = re.search(version_pattern, open(filename).read(), re.MULTILINE)
+
+    assert match, ("No version found in file: {!r} matching pattern: {!r}"
+                   .format(filename, version_pattern))
+
+    return match.group(1)
+
 
 setup(
     name="django-cam-auth-utils",
-    version="0.0.1",
+    version=get_version("django_cam_auth_utils/__init__.py"),
     description=get_readme_text(),
     author="Hal Blackburn",
     author_email="hal@caret.cam.ac.uk",
